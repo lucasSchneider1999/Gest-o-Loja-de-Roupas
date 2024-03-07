9669c72d5422730b0c5d6f6339f6edfc4b5d51d9
gustavo <gustavoronn@gmail.com>
2024-03-07 19:08:45 -0300

    Relationship setted on vendas

--- src/main/java/app/entity/Venda.java
+++ src/main/java/app/entity/Venda.java
@@ -1,18 +1,46 @@
 package app.entity;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Venda {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long idVenda;
 	private String enderecoVenda;
 	private int valorVenda;
+	
+	@ManyToOne (cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("venda")
+	private Funcionario funcionario;
+	
+	@ManyToOne (cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("venda")
+	private Cliente cliente;
+	
+	ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable (name =  "venda_produto")
+	private List<Produto> produto;
+	
 	
 }
